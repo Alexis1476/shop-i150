a5f41c43b0b397a6cc7b8e895cc02ba4420df98a
Alexis1476 <halexisr20@hotmail.com>
2022-09-05 16:24:33 +0200

    Calcul sous-total et total

--- view/page/basket/list.php
+++ view/page/basket/list.php
@@ -13,6 +13,7 @@
             </tr>
             </thead>
             <tbody>
+            <?php $total = 0; ?>
             <?php if (isset($products) & $products != null): ?>
                 <?php foreach ($products as $product) : ?>
                     <tr>
@@ -21,8 +22,11 @@
                         <td><!--TODO: Gerer quantité-->
                             <?= $_SESSION['products'][$product[0]['idProduct']] ?>
                         </td>
-                        <td>
-                            CHF <?= $_SESSION['products'][$product[0]['idProduct']] * $product[0]['proPrice'] ?>
+                        <td>CHF
+                            <?php
+                            $sousTotal = $_SESSION['products'][$product[0]['idProduct']] * $product[0]['proPrice'];
+                            $total += $sousTotal;
+                            echo $sousTotal ?>
                             <a href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
                                     <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/>
@@ -44,7 +48,7 @@
                 <td>Total</td>
                 <td></td>
                 <td></td>
-                <td><!--TODO: Prix total--></td>
+                <td>CHF <?= $total ?></td>
             </tr>
             </tfoot>
         </table>

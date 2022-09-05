127bd65dcee4c5edba74c333474ef4439d9eca1d
Alexis1476 <halexisr20@hotmail.com>
2022-09-05 15:52:56 +0200

    Ajout des boutons modifier et supprimer

--- view/page/basket/list.php
+++ view/page/basket/list.php
@@ -22,7 +22,18 @@
                             <?= $_SESSION['products'][$product[0]['idProduct']] ?>
                         </td>
                         <td>
-                            <!--TODO: Ajouter boutons-->
+                            CHF <?= $_SESSION['products'][$product[0]['idProduct']] * $product[0]['proPrice'] ?>
+                            <a href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+                                    <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/>
+                                </svg>
+                            </a>
+                            <a href="#">
+                                <svg width="22" height="22" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                                     clip-rule="evenodd">
+                                    <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/>
+                                </svg>
+                            </a>
                         </td>
                     </tr>
                 <?php endforeach; ?>

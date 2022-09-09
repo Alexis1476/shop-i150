c8f5f4b09add3bd7b876d015dae407a26e26fc2f
Alexis1476 <halexisr20@hotmail.com>
2022-09-09 15:21:09 +0200

    Page mÃoyen de livraison

---
+++ view/page/basket/delivery.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <h3>Choisir un moyen de livraison</h3>
+    <form action="#">
+        <div class="input-group">
+            <input type="radio" id="poste" name="delivery-method">
+            <label for="poste">Par la poste (+ CHF <!--TODO, valeur dynamique-->)</label>
+        </div>
+        <div class="input-group">
+            <input type="radio" id="poste" name="delivery-method">
+            <label for="poste">Retrait au magasin</label>
+        </div>
+        <button class="btn btn-default">Suivant</button>
+    </form>
+</div>

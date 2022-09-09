7c8972f102150c45fd01108461f91d77650cdcdd
Alexis1476 <halexisr20@hotmail.com>
2022-09-09 16:21:18 +0200

    Formulaire adresse

--- view/page/order/addresse.php
+++ view/page/order/addresse.php
@@ -1,10 +1,37 @@
 <div class="container">
     <h3>Choisir un moyen de livraison</h3>
-    <form action="index.php?controller=order&action=payment" method="post">
-        <div class="input-group">
-            <label>
-                <input type="text">
-            </label>
+    <form action="index.php?controller=order&action=#" method="post">
+        <div class="form-group">
+            <label for="titre">Titre</label>
+            <input name="titre" type="text" class="form-control" id="titre"/>
+        </div>
+        <div class="form-group">
+            <label for="nom">Nom</label>
+            <input name="nom" type="text" class="form-control" id="nom"/>
+        </div>
+        <div class="form-group">
+            <label for="prenom">Prénom</label>
+            <input name="prenom" type="text" class="form-control" id="prenom"/>
+        </div>
+        <div class="form-group">
+            <label for="rue">Rue</label>
+            <input name="rue" type="text" class="form-control" id="rue"/>
+        </div>
+        <div class="form-group">
+            <label for="numero-rue">N°</label>
+            <input name="numero-rue" type="text" class="form-control" id="numero-rue"/>
+        </div>
+        <div class="form-group">
+            <label for="npa">NPA</label>
+            <input name="npa" type="text" class="form-control" id="npa"/>
+        </div>
+        <div class="form-group">
+            <label for="localite">Localité</label>
+            <input name="localite" type="text" class="form-control" id="localite"/>
+        </div>
+        <div class="form-group">
+            <label for="mail">Adresse mail</label>
+            <input name="mail" type="text" class="form-control" id="mail"/>
         </div>
         <button class="btn btn-default">Suivant</button>
     </form>

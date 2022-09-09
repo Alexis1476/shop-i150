213eb921b52f654741b5a608a592ca662bf584a1
Alexis1476 <halexisr20@hotmail.com>
2022-09-09 15:53:37 +0200

    Ajout methode + action au formulaire

--- view/page/order/delivery.php
+++ view/page/order/delivery.php
@@ -1,12 +1,12 @@
 <div class="container">
     <h3>Choisir un moyen de livraison</h3>
-    <form action="#">
+    <form action="index.php?controller=order&action=payment" method="post">
         <div class="input-group">
-            <input type="radio" id="poste" name="delivery-method">
+            <input type="radio" id="poste" name="delivery-method" value="<?= $postePrice ?>">
             <label for="poste">Par la poste (+ CHF <?= $postePrice ?>)</label>
         </div>
         <div class="input-group">
-            <input type="radio" id="poste" name="delivery-method">
+            <input type="radio" id="poste" name="delivery-method" value="0">
             <label for="poste">Retrait au magasin</label>
         </div>
         <button class="btn btn-default">Suivant</button>

1d3a1e737cfc73bd829d599bef89127769fb448a
Alexis1476 <halexisr20@hotmail.com>
2022-09-12 14:39:47 +0200

    Squelette page de récapitulatif

---
+++ view/page/order/summary.php
@@ -0,0 +1,49 @@
+<div class="container">
+    <h3>Récapitulatif</h3>
+    <div class="justify-content">
+        <p>Envoyé à :</p>
+        <p>Livraison :</p>
+        <p>Paiement :</p>
+    </div>
+    <p><!--TODO: Titre--></p>
+    <p><!--TODO: Prenom Nom--></p>
+    <p><!--TODO: Adresse N°--></p>
+    <p><!--TODO: NPA Localite--></p>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Description</th>
+            <th>Prix</th>
+            <th>Quantité</th>
+            <th>Sous-total</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!--TODO : Boucle pour afficher les produits-->
+        <tr>
+            <td><!--TODO : Nom moyen de paiement--></td>
+            <td></td>
+            <td></td>
+            <td>CHF <!--TODO : Prix moyen de paiement--></td>
+        </tr>
+        <tr>
+            <td>Total</td>
+            <td></td>
+            <td></td>
+            <td>CHF <!--TODO : Total produits--></td>
+        </tr>
+        <tr>
+            <td><!--TODO : Moyen de paiement--></td>
+            <td></td>
+            <td></td>
+            <td>CHF <!--TODO : Calcul montant moyen de paiement--></td>
+        </tr>
+        <tr>
+            <td>Total à payer</td>
+            <td></td>
+            <td></td>
+            <td>CHF <!--TODO : Total--></td>
+        </tr>
+        </tbody>
+    </table>
+</div>

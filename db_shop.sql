8e4cee182bef097cdad7d4bab456d75095f175c9
Alexis1476 <halexisr20@hotmail.com>
2022-10-02 20:39:00 +0200

    SQL script udpate

--- db_shop.sql
+++ db_shop.sql
@@ -1,147 +1,216 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Mer 29 Août 2018 à 07:26
--- Version du serveur :  5.7.11
--- Version de PHP :  7.0.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- *********************************************
+-- * SQL MySQL generation                      
+-- *--------------------------------------------
+-- * DB-MAIN version: 11.0.2              
+-- * Generator date: Sep 14 2021              
+-- * Generation date: Sun Oct  2 20:33:29 2022 
+-- * LUN file: E:\Applications\UwAmp\www\shop-i150\db_shop_150.lun 
+-- * Schema: mld/1-1 
+-- ********************************************* 
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Database Section
+-- ________________ 
 
---
--- Base de données :  `db_shop`
---
+create database db_shop;
+use db_shop;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `t_category`
---
+-- Tables Section
+-- _____________ 
 
-CREATE TABLE `t_category` (
-  `idCategory` int(11) NOT NULL,
-  `catName` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
+create table t_category (
+     idCategory bigint not null auto_increment,
+     catName varchar(30) not null,
+     constraint ID_t_category_ID primary key (idCategory));
+     --
 -- Contenu de la table `t_category`
 --
+INSERT INTO `t_category` (`idCategory`, `catName`)
+VALUES (1, 'Vêtement'),
+     (2, 'Sport'),
+     (3, 'Informatique');
 
-INSERT INTO `t_category` (`idCategory`, `catName`) VALUES
-(1, 'Vêtement'),
-(2, 'Sport'),
-(3, 'Informatique');
+create table t_concern (
+     fkOrder bigint not null,
+     fkProduct bigint not null,
+     conQuantity char(1) not null,
+     constraint ID_t_concern_ID primary key (fkOrder, fkProduct));
 
--- --------------------------------------------------------
+create table t_order (
+     idOrder bigint not null auto_increment,
+     ordPaid char not null,
+     ordTitle varchar(50) not null,
+     ordFirstName varchar(50) not null,
+     ordLastName varchar(50) not null,
+     ordStreet varchar(100) not null,
+     ordStreetNumber varchar(4) not null,
+     ordLocality varchar(50) not null,
+     ordMail varchar(255) not null,
+     ordPhoneNumber varchar(20) not null,
+     fkOrderStatus bigint,
+     constraint ID_t_order_ID primary key (idOrder));
 
---
--- Structure de la table `t_product`
---
+create table t_orderStatus (
+     idOrderStatus bigint not null auto_increment,
+     ordName varchar(50) not null,
+     constraint ID_t_orderStatus_ID primary key (idOrderStatus));
 
-CREATE TABLE `t_product` (
-  `idProduct` int(11) NOT NULL,
-  `proName` varchar(50) NOT NULL,
-  `proDescription` text,
-  `proPrice` float DEFAULT NULL,
-  `proQuantity` smallint(6) DEFAULT NULL,
-  `proImage` text,
-  `proLike` int(11) NOT NULL DEFAULT '0',
-  `fkCategory` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table t_product (
+     idProduct bigint not null auto_increment,
+     proName varchar(50) not null,
+     proDescription varchar(255) not null,
+     proPrice float(1) not null,
+     proQuantity int not null,
+     proImage varchar(255) not null,
+     proLike bigint not null,
+     fkCategory bigint not null,
+     constraint ID_t_product_ID primary key (idProduct));
 
---
+     --
 -- Contenu de la table `t_product`
 --
+INSERT INTO `t_product` (
+          `idProduct`,
+          `proName`,
+          `proDescription`,
+          `proPrice`,
+          `proQuantity`,
+          `proImage`,
+          `proLike`,
+          `fkCategory`
+     )
+VALUES (
+          1,
+          'Pullover',
+          'Pullover synthétique',
+          20.9,
+          10,
+          'pull.png',
+          0,
+          1
+     ),
+     (
+          2,
+          'Pantalon',
+          'Pantalon en Jean\'s véritable',
+          49.9,
+          25,
+          'pants.png',
+          0,
+          1
+     ),
+     (
+          3,
+          'Clé Usb ',
+          'Clé Usb de 4 Go offrant une fiabilité inégalée.',
+          4.95,
+          4,
+          'usb.png',
+          10,
+          3
+     ),
+     (
+          4,
+          'Disque dur externe',
+          'Disque dur externe de 1 To, idéal pour stocker et sauvegarder vos données de manière sécurisée.',
+          79.95,
+          2,
+          'harddisk.png',
+          0,
+          3
+     ),
+     (
+          5,
+          'T-Shirt Fox',
+          NULL,
+          14.9,
+          50,
+          'shirt1.png',
+          0,
+          2
+     ),
+     (
+          6,
+          'T-Shirt Mignon',
+          NULL,
+          14.9,
+          20,
+          'shirt2.png',
+          0,
+          2
+     );
 
-INSERT INTO `t_product` (`idProduct`, `proName`, `proDescription`, `proPrice`, `proQuantity`, `proImage`, `proLike`, `fkCategory`) VALUES
-(1, 'Pullover', 'Pullover synthétique', 20.9, 10, 'pull.png', 0, 1),
-(2, 'Pantalon', 'Pantalon en Jean\'s véritable', 49.9, 25, 'pants.png', 0, 1),
-(3, 'Clé Usb ', 'Clé Usb de 4 Go offrant une fiabilité inégalée.', 4.95, 4, 'usb.png', 10, 3),
-(4, 'Disque dur externe', 'Disque dur externe de 1 To, idéal pour stocker et sauvegarder vos données de manière sécurisée.', 79.95, 2, 'harddisk.png', 0, 3),
-(5, 'T-Shirt Fox', NULL, 14.9, 50, 'shirt1.png', 0, 2),
-(6, 'T-Shirt Mignon', NULL, 14.9, 20, 'shirt2.png', 0, 2);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `t_user`
---
-
-CREATE TABLE `t_user` (
-  `idUser` int(11) NOT NULL,
-  `useLogin` varchar(20) NOT NULL,
-  `usePassword` text NOT NULL,
-  `useRight` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+create table t_user (
+     idUser bigint not null auto_increment,
+     useLogin varchar(20) not null,
+     usePassword varchar(255) not null,
+     useRight varchar(10) not null,
+     constraint ID_t_user_ID primary key (idUser));
 --
 -- Contenu de la table `t_user`
 --
+INSERT INTO `t_user` (`idUser`, `useLogin`, `usePassword`, `useRight`)
+VALUES (
+          1,
+          'admin',
+          '$2y$10$UZ6NOFmrq5QY.XDdicl8EO0vxy.D/d4H48GrbRdTcKYQ0Kwqoo.ie',
+          'admin'
+     ),
+     (
+          2,
+          'customer',
+          '$2y$10$.8sXz4/qH5sngNgdEnn8HesHO3qoDvUcYmslZyGIpCUz1He0OmOAS',
+          'customer'
+     );
 
-INSERT INTO `t_user` (`idUser`, `useLogin`, `usePassword`, `useRight`) VALUES
-(1, 'admin', '$2y$10$UZ6NOFmrq5QY.XDdicl8EO0vxy.D/d4H48GrbRdTcKYQ0Kwqoo.ie', 'admin'),
-(2, 'customer', '$2y$10$.8sXz4/qH5sngNgdEnn8HesHO3qoDvUcYmslZyGIpCUz1He0OmOAS', 'customer');
+-- Constraints Section
+-- ___________________ 
 
---
--- Index pour les tables exportées
---
+alter table t_concern add constraint FKt_c_t_p_FK
+     foreign key (fkProduct)
+     references t_product (idProduct);
 
---
--- Index pour la table `t_category`
---
-ALTER TABLE `t_category`
-  ADD PRIMARY KEY (`idCategory`);
+alter table t_concern add constraint FKt_c_t_o
+     foreign key (fkOrder)
+     references t_order (idOrder);
 
---
--- Index pour la table `t_product`
---
-ALTER TABLE `t_product`
-  ADD PRIMARY KEY (`idProduct`),
-  ADD KEY `Index_Category` (`fkCategory`);
+alter table t_order add constraint FKt_be_FK
+     foreign key (fkOrderStatus)
+     references t_orderStatus (idOrderStatus);
 
---
--- Index pour la table `t_user`
---
-ALTER TABLE `t_user`
-  ADD PRIMARY KEY (`idUser`);
+alter table t_product add constraint FKt_have_FK
+     foreign key (fkCategory)
+     references t_category (idCategory);
 
---
--- AUTO_INCREMENT pour les tables exportées
---
 
---
--- AUTO_INCREMENT pour la table `t_category`
---
-ALTER TABLE `t_category`
-  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT pour la table `t_product`
---
-ALTER TABLE `t_product`
-  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT pour la table `t_user`
---
-ALTER TABLE `t_user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- Contraintes pour les tables exportées
---
+-- Index Section
+-- _____________ 
 
---
--- Contraintes pour la table `t_product`
---
-ALTER TABLE `t_product`
-  ADD CONSTRAINT `t_product_ibfk_1` FOREIGN KEY (`fkCategory`) REFERENCES `t_category` (`idCategory`);
+create unique index ID_t_category_IND
+     on t_category (idCategory);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+create unique index ID_t_concern_IND
+     on t_concern (fkOrder, fkProduct);
+
+create index FKt_c_t_p_IND
+     on t_concern (fkProduct);
+
+create unique index ID_t_order_IND
+     on t_order (idOrder);
+
+create index FKt_be_IND
+     on t_order (fkOrderStatus);
+
+create unique index ID_t_orderStatus_IND
+     on t_orderStatus (idOrderStatus);
+
+create unique index ID_t_product_IND
+     on t_product (idProduct);
+
+create index FKt_have_IND
+     on t_product (fkCategory);
+
+create unique index ID_t_user_IND
+     on t_user (idUser);
+

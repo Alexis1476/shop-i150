16398e71c1497251151a9725da4e9e50095a3d10
Alexis1476 <halexisr20@hotmail.com>
2022-10-03 08:20:13 +0200

    Delivery methods created

---
+++ data/deliveryMethods.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Auteur : Alexis Rojas
+ * Date: 03.10.2022
+ * Moyens de paiement
+ */
+
+ $deliveryMethods = [
+    0 => [
+        'id' => 1,
+        'name' => 'Poste',
+        'text' => 'Par la poste (+ CHF 7.95)',
+        'operator' => '+',
+        'value' => 7.95
+    ],
+    1 => [
+        'id' => 2,
+        'name' => 'Retrait',
+        'text' => 'Retrait au magasin',
+        'operator' => '+',
+        'value' => 0
+    ]
+ ];

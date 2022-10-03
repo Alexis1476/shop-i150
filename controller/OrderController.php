7778efc5756dc9377e1bafa675a95e351eb363e0
Alexis1476 <halexisr20@hotmail.com>
2022-10-03 09:20:53 +0200

    Validation of addresse form

--- controller/OrderController.php
+++ controller/OrderController.php
@@ -73,4 +73,63 @@ class OrderController extends Controller
 
         return $content;
     }
+    public function addresse()
+    {
+        $errors = [];
+        // Si le serveur traite une méthode post
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+            define('ERROR_MESSAGE', 'Remplissez ce champ correctement');
+            // Validation des données
+            if (!$_POST['title'] || !preg_match("/^[a-z|A-Z]+$/", $_POST['title'])) {
+                $errors['title'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['firstName'] || !preg_match("/^[a-z|A-Z]+$/", $_POST['firstName'])) {
+                $errors['firstName'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['lastName'] || !preg_match("/^[a-z|A-Z]+$/", $_POST['lastName'])) {
+                $errors['lastName'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['street'] || !preg_match("/^[a-z |A-Z0-9]+$/", $_POST['street'])) {
+                $errors['street'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['streetNumber'] || !preg_match("/^[0-9]+$/", $_POST['streetNumber'])) {
+                $errors['streetNumber'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['pc'] || !preg_match("/^[0-9]{4}$/", $_POST['pc'])) {
+                $errors['pc'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['locality'] || !preg_match("/^[a-z |A-Z]+$/", $_POST['locality'])) {
+                $errors['locality'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['mail'] || !preg_match("/^\S+@\S+\.[A-Z|a-z]+$/", $_POST['mail'])) {
+                $errors['mail'] = ERROR_MESSAGE;
+            }
+            if (!$_POST['phone'] || !preg_match("/^(\+41|0)7([0-9]{8})$/", $_POST['phone'])) {
+                $errors['phone'] = ERROR_MESSAGE;
+            }
+            // Recupère les données dans la session
+            $_SESSION['title'] = $_POST['title'];
+            $_SESSION['firstName'] = $_POST['firstName'];
+            $_SESSION['lastName'] = $_POST['lastName'];
+            $_SESSION['street'] = $_POST['street'];
+            $_SESSION['streetNumber'] = $_POST['streetNumber'];
+            $_SESSION['pc'] = $_POST['pc'];
+            $_SESSION['locality'] = $_POST['locality'];
+            $_SESSION['mail'] = $_POST['mail'];
+            $_SESSION['phone'] = $_POST['phone'];
+
+            // S'il n'y a pas d'erreurs
+            if (!$errors) {
+                header('Location: index.php?controller=order&action=summary');
+            }
+        }
+
+        $view = file_get_contents('view/page/order/addresse.php');
+
+        ob_start();
+        eval('?>' . $view);
+        $content = ob_get_clean();
+
+        return $content;
+    }
 }

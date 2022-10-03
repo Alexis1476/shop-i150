f8c0e57326ea165985fa61a0ebe732fbad2405a9
Alexis1476 <halexisr20@hotmail.com>
2022-10-03 08:48:02 +0200

    Format of payment method modified

--- data/paymentMethods.php
+++ data/paymentMethods.php
@@ -10,7 +10,7 @@
     0 => [
         'id' => 1,
         'name' => 'Facture',
-        'text' => 'Sur facture (+2.-)',
+        'text' => 'Sur facture (+ 2.-)',
         'operator' => '+',
         'value' => 2
     ],
